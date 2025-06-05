ee71b98c1f22140a68ea915fc61a6e2f198d60b6
Neo <iladorovs@yandex.ru>
2025-06-05 18:48:36 +0600

    fix: fix path to app android icon and rebuild app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,5 @@
 import {Slot} from 'expo-router';
 import {ReduxProvider} from '@entrypoint/providers/redux-provider';
-import {BootSplashProvider} from '@entrypoint/providers/bootsplash-provider';
 
 export default function RootLayout() {
   return (
